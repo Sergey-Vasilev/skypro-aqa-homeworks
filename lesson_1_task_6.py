030cb5897cbc787e49eeec688d112d0c23b8a79d
Sergey-Vasilev <sergeyvasilev251@gmail.com>
2023-06-19 11:07:13 +0500

    решения

---
+++ lesson_1_task_6.py
@@ -0,0 +1,33 @@
+def a_output():
+    print(1) 
+def b_output():
+    print(2)
+def c_output():
+    print(3) 
+def d_output():
+    print(4) 
+def e_output():
+    print(5) 
+def f_output():
+    print(6) 
+def g_output():
+    print(7) 
+def h_output():
+    print(8) 
+def i_output():
+    print(9) 
+def j_output():
+    print(0)  
+
+
+h_output()
+h_output()
+j_output()
+j_output()
+e_output()
+e_output()
+e_output()
+c_output()
+e_output()
+c_output()
+e_output()
