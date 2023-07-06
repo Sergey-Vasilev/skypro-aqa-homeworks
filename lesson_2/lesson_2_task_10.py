39aaa21eca0b8f3bebaaba5d3917bbafc2bdb774
Sergey-Vasilev <sergeyvasilev251@gmail.com>
2023-07-06 14:31:12 +0500

    Решение

--- lesson_2/lesson_2_task_10.py
+++ lesson_2/lesson_2_task_10.py
@@ -1,16 +1,16 @@
 
 def bank(X, Y):
-    total = X  # Изначально сумма равна X
+    total = X  
 
     for i in range(Y):
-        total += total * 0.1  # Увеличение суммы на 10% каждый год
+        total += total * 0.1  
 
     return total
-X = int(input('введите сумму вклада:'))  # Размер вклада в рублях
-Y = int(input('ввеите на сколько лет вклад в годах:'))   # Срок вклада в годах
+X = int(input('введите сумму вклада:'))  
+Y = int(input('введите на сколько лет вклад (в годах):'))   
 
 result = bank(X, Y)
-print(result)  # Выводит сумму на счету пользователя спустя 5 лет
+print(result) 
 
 
 
